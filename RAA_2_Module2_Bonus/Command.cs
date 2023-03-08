3cbf01fb19f31454f6f00f3395e684b6a1a56cf5
kilkellym <michael@archsmarter.com>
2023-03-08 15:45:04 -0500

    progress

--- RAA_2_Module2_Bonus/Command.cs
+++ RAA_2_Module2_Bonus/Command.cs
@@ -22,15 +22,9 @@ namespace RAA_2_Module2_Bonus
           ElementSet elements)
         {
             UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
-            Document doc = uidoc.Document;
-
-            // put any code needed for the form here
-            List<Category> categories = Utils.GetAllCategories(doc);
 
             // open form
-            MyForm currentForm = new MyForm(doc, categories)
+            MyForm currentForm = new MyForm(uiapp)
             {
                 Width = 450,
                 Height = 550,
@@ -39,43 +33,6 @@ namespace RAA_2_Module2_Bonus
             };
 
             currentForm.ShowDialog();
-
-            // get form data and do something
-            if (currentForm.DialogResult == false)
-                return Result.Cancelled;
-
-            List<Parameter> paramList = Utils.GetParametersByName(doc, currentForm.CategoryName, currentForm.TypeNames,
-                currentForm.ParamName);
-
-            if(paramList.Count > 0)
-            {
-                Transaction t = new Transaction(doc);
-                t.Start("Set type parameters");
-
-                foreach(Parameter param in paramList)
-                {
-                    string newValue = currentForm.GetNewValue();
-
-                    if(currentForm.ParamDataType == "double")
-                    {
-                        double paramDouble = Convert.ToDouble(newValue);
-                        param.Set(paramDouble);
-                    }
-                    else if(currentForm.ParamDataType == "integer")
-                    {
-                        int paramInt = Convert.ToInt32(newValue);
-                        param.Set(paramInt);
-                    }
-                    else if(currentForm.ParamDataType == "string")
-                    {
-                        param.Set(newValue);
-                    }
-                }
-                t.Commit();
-                t.Dispose();
-            }
-
-            TaskDialog.Show("Complete", "Updated " + paramList.Count.ToString() + " types.");
 
             return Result.Succeeded;
         }
